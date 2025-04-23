15cb7397a3eb6dd546b8032f6dbb4eccf9585033
surajpxl <guptasuraj4455@gmail.com>
2025-04-23 20:19:57 +0530

    frontend complete almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Freebook from './components/Freebook'
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-    <Navbar/>
-    <Banner/>
-    <Footer/>
-    <Freebook/>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
