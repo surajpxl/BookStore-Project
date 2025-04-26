0da79c072e4e098e3dcd2282d28164933974024e
surajpxl <guptasuraj4455@gmail.com>
2025-04-26 20:38:19 +0530

    frontend work

--- forntendd/src/App.jsx
+++ forntendd/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
+
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
