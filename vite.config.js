c8909c7d944d19e332c7aad7f09b523f573ff9de
surajpxl <guptasuraj4455@gmail.com>
2025-04-26 20:06:34 +0530

    Added frontend folder into main bookStore project

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import daisyui from 'daisyui'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
